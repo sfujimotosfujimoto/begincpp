94be95e8a0be59c760fb784414a765f0b74e405f
S Fujimoto <unicoshun@gmail.com>
2018-05-02 21:00:52 +0900

    Initial Commit

---
+++ BeginningCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BeginningCPP
+//
+//  Created by s fujimoto on 2018/05/02.
+//  Copyright © 2018 sfujimoto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
